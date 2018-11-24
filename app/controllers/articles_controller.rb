57223d4397306f7be44f26d1c1d60fee731a0c7b
hanna_nyman <hannanyman.sk@gmail.com>
2018-11-24 15:02:03 +0100

    Adding HTML for editing articles, showing articles and deleting articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,48 @@
 class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to welcome_index_path
+  end
+
   def index
+    @article = Article.new
     @articles = Article.all
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated"
+      redirect_to article_path(@article)
+    else
+      flash[:notice] = "Article was not updated"
+      render 'edit'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to welcome_index_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
 end
